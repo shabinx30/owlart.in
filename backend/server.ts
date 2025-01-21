582457b30587800f73c7a31357685619bda7521c
shabinx30 <shabeensharih@gmail.com>
2025-01-21 14:39:37 +0530

    server setup completed

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,11 @@
-console.log('test')
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors({ origin: "http://localhost:5173" }))
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`server running on ${process.env.PORT || 4000}`)
+})
